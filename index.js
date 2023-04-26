5a929d354379bdac83717300ab9415814c128f7b
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-04-26 15:27:45 +0100

    Add index.js file for my counter app iteractivity

---
+++ index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+const saveEl = document.getElementById('save-el');
+const countEl = document.getElementById('count-el');
+
+let count = 0;
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  const countStr = `${count} - `;
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+}
